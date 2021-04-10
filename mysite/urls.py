01c63e6be70c8b139ac5a6fb6b4930f4a75ce658
Seugwoo Yoo <seungwoo@deepnatural.ai>
2021-04-10 16:10:30 +0900

    Add index view and URLconf

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls'))
+    # The include() function allows referencing other URLconfs.
+    # Whenever Django encounters include(), it chops off whatever part of the URL matched up to that point
+    # and sends the remaining string to the included URLconf for further processing.
 ]
