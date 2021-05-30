62f2b3e18c2da3751d2d666ba89b4b120c3cc5c0
Seugwoo Yoo <seungwoo@deepnatural.ai>
2021-05-31 01:31:26 +0900

    refactor index view using generic.ListView

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,17 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
 def detail(request, question_id):
