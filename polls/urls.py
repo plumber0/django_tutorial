26928fa664a0f5cc6a95ebddc80281f7a86e056a
Seugwoo Yoo <seungwoo@deepnatural.ai>
2021-05-31 01:34:00 +0900

    refactor detail view using generic.DetailView

--- polls/urls.py
+++ polls/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # ex: /polls/
     path('', views.IndexView.as_view(), name='index'),
     # ex : /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/', views.DetailView, name='detail'),
     # ex : /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex : /polls/5/vote/
